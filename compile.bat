624bf6e903bbe0ac5460b5f62a18b9837fc3551e
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2019-12-24 16:13:23 -0500

    Add Gregar support.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 mkdir "temp" 2> nul
 tools\armips src.s -strequ ver "bn6f.s"
 if errorlevel 1 pause
-::tools\armips src.s -strequ ver "bn6g.s"
-::if errorlevel 1 pause
+tools\armips src.s -strequ ver "bn6g.s"
+if errorlevel 1 pause
